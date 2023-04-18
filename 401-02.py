8f31c6f8b699be7f40a7abc82bfb7a17e5709574
nkalderete <nk.alderete@gmail.com>
2023-04-18 21:44:26 +0000

    401 Code Challenge, class 02

---
+++ 401-02.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# Nick Alderete
+
+
+
+import os
+import datetime
+import time
+# from datetime import datetime
+
+target = "8.8.8.8"
+
+while True:
+    # Preform a single ping packet to the target
+    response = os.system("ping -c 1 " + target)
+
+    # Evaluate the response and assign success or failure to the status variable
+    if response == 0:
+        status = "Success"
+    else:
+        status = "Failure"
+
+    # Get the current timestamp and print the status and timestamp
+    current_time = datetime.datetime.now()
+    print(f"{current_time} - Status: {status}")
+    
+    # Wait for 2 seconds before transmitting another ping packet
+    time.sleep(2)
