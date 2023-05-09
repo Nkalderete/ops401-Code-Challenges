4b057a82e1a4a92d7b3b6a51111dffeaeab0e07e
nick <nk.alderete@gmail.com>
2023-05-09 00:22:12 +0000

    401 Ops Challenge, Class 16

--- 401-16.py
+++ 401-16.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 # Nick Aldereet
 # Worked with Geneva and Sierra
+# Sources: 
+    # https://www.shiksha.com/online-courses/articles/validating-passwords-using-python-regex-match/
+    # https://www.geeksforgeeks.org/password-validation-in-python/#     
 
 
 import time
+import re 
 
 def mode1():
     # Ask user for wordlist file path
@@ -32,14 +36,60 @@ def mode2():
                 print("Your password is beefy enough!")
                 break
 
+def mode3():
+    # Ask user for password
+    password = input("Please enter your password: \n")
+        # Check if password meets length requirement
+    if len(password) >= 6:
+        length_check = True
+    else:
+        length_check = False
+    
+    # Check if password has at least 1 capital letter
+    if re.search('[A-Z]', password):
+        capital_check = True
+    else:
+        capital_check = False
+    
+    # Check if password has at least 2 numbers
+    if len(re.findall('\d', password)) >= 2:
+        number_check = True
+    else:
+        number_check = False
+    
+    # Check if password has at least 1 symbol
+    if re.search('[^a-zA-Z0-9]', password):
+        symbol_check = True
+    else:
+        symbol_check = False
+    
+    # Print which password complexity metrics were satisfied
+    if length_check and capital_check and number_check and symbol_check:
+        print("SUCCESS! Password meets beefy requirements.")
+    else:
+        print("Beefy password requirements NOT met. The following requirements MUST be met:\n")
+        if not length_check:
+            print("Please use at least 6 characters\n")
+        if not capital_check:
+            print("Please use at least 1 capital letter\n")
+        if not number_check:
+            print("Please use at least 2 numbers\n")
+        if not symbol_check:
+            print("Please use at least 1 symbol\n")
+
 # User menu
 while True:
-    answer = input("Select Mode 1 or Mode 2: ")
+    answer = input("Select Mode 1, Mode 2 or Mode 3: ")
     if answer == "Mode 1":
         mode1()
         break
     elif answer == "Mode 2":
         mode2()
         break
+    elif answer == "Mode 3":
+        mode3()
+        break
     else:
         print("Invalid mode selection. Please try again.")
+
+# END
