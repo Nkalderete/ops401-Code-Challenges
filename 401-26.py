d007a033860c006262ca02d34361ac5c1e26375e
nick <nk.alderete@gmail.com>
2023-05-23 20:31:47 +0000

    Uptime sensor with logging | 1 of 3

--- 401-26.py
+++ 401-26.py
@@ -10,20 +10,29 @@ import time
 
 
 
-
 target = input("Enter the target IP address: \n")
+
+
+# Create and configure logger
+logging.basicConfig(filename="Demo.log", format='%(asctime)s %(message)s', filemode='w')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
 def ping_status(target):
     try:
-        # Evaluate the response and assign success or failure to the status 
+        # Check ping status and return success or failure
         icmp = os.system("ping -c 1 " + target)
         if icmp == 0:
-            status = "Successful ping."
+            status = "Successful ping, network is responding."
             print(f"{target} is up and responding.")
+            #Log successful ping
+            logger.info(f"\nIP address: {target} \nStatus: {status}")  
         else:
             status = "Failed ping, check your IP address or network is down."
             print(f"{target} is not responding, try again later or check IP address.")
+            # Log failed ping
+            logger.warning(f"\nIP address: {target} \nStatus: {status}")  
         
         # Get the current timestamp and print the status and timestamp
         current_time = datetime.datetime.now()
@@ -33,23 +42,12 @@ def ping_status(target):
         logging.exception("An error occurred")
         raise e
 
-# Create and configure logger
-logging.basicConfig(filename="Demo.log", format='%(asctime)s %(message)s', filemode='w')
-
-# Create object
-logger = logging.getLogger()
-
-# Setting the threshold
-logger.setLevel(logging.DEBUG)
-
 while True:
     try:
         # Transmit a single ICMP ping packet to the target
         ping_status(target)
         time.sleep(2)
-        
     except KeyboardInterrupt:
         break
-    
     except Exception as e:
         logging.exception("An error occurred")
